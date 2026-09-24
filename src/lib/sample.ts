414d64edddf8c96b2f4d8b4f924e476197ad6519
AthulKrishnaAV47 <athulkrishnaav171@gmail.com>
2026-09-24 13:50:54 +0530

    feat: add sample slot sheet and course eligibility datasets

--- src/lib/sample.ts
+++ src/lib/sample.ts
@@ -131,12 +131,295 @@ export const SAMPLE_ELIGIBILITY = `Course Code | SBC or FC | Year
 19AI301     | SBC       | I
 19AI302     | FC        | I
 19AI303     | FC        | I
+19AI304     | SBC       | I
+19AI305     | SBC       | I
+19AI307     | SBC       | I
+19AI403     | FC        | I
+19AI405     | SBC       | I
+19AI407     | FC        | I
+19AI410     | SBC       | I
+19AI414     | SBC       | I
+19BM301     | SBC       | I
+19BM803     | SBC       | I
+19CE803     | SBC       | I
+19CE810     | SBC       | I
 19CS305     | FC        | I
-19CS305     | FC        | II & III
-19EE305     | FC        | I
-19AI305     | FC        | I
-19JP301     | SBC       | I
+19CS405     | SBC       | I
+19CS406     | SBC       | I
+19CS547     | FC        | I
+19CY205     | FC        | I
+19CY208     | FC        | I
 19CY801     | FC        | I
+19CY802     | FC        | I
+19CY805     | FC        | I
+19EC302     | FC        | I
+19EC402     | FC        | I
+19EC408     | SBC       | I
+19EC419     | SBC       | I
+19EC801     | SBC       | I
+19EC802     | FC        | I
+19EE301     | SBC       | I
+19EE305     | FC        | I
+19EE306     | FC        | I
+19EE404     | FC        | I
+19EE411     | SBC       | I
+19EE804     | SBC       | I
+19EN101     | SBC       | I
+19EN106     | SBC       | I
+19EN107     | SBC       | I
+19EN616     | SBC       | I
+19EY708     | SBC       | I
 19HS801     | FC        | I
-19AI410     | FC        | II & III
-19DE301     | SBC       | II & III`;
+19MA201     | FC        | I
+19MA211     | FC        | I
+19MA212     | FC        | I
+19MA220     | FC        | I
+19MA222     | FC        | I
+19MA223     | FC        | I
+19MA224     | FC        | I
+19ME414     | FC        | I
+19PH211     | FC        | I
+19PH216     | FC        | I
+19PH802     | FC        | I
+19PH814     | FC        | I
+19TD603     | FC        | I
+19TD605     | FC        | I
+19TD608     | FC        | I
+19AG305     | FC        | II & III
+19AG410     | FC        | II & III
+19AG411     | FC        | II & III
+19AG415     | FC        | II & III
+19AG417     | FC        | II & III
+19AG418     | FC        | II & III
+19AG419     | SBC       | II & III
+19AG422     | FC        | II & III
+19AG816     | FC        | II & III
+19AI301     | SBC       | II & III
+19AI302     | FC        | II & III
+19AI303     | FC        | II & III
+19AI304     | SBC       | II & III
+19AI305     | SBC       | II & III
+19AI307     | SBC       | II & III
+19AI403     | FC        | II & III
+19AI404     | SBC       | II & III
+19AI405     | SBC       | II & III
+19AI406     | FC        | II & III
+19AI407     | FC        | II & III
+19AI408     | SBC       | II & III
+19AI409     | SBC       | II & III
+19AI410     | SBC       | II & III
+19AI411     | SBC       | II & III
+19AI412     | SBC       | II & III
+19AI413     | SBC       | II & III
+19AI414     | SBC       | II & III
+19AI502     | FC        | II & III
+19AI505     | SBC       | II & III
+19AI509     | FC        | II & III
+19AI513     | FC        | II & III
+19AI516     | FC        | II & III
+19AI534     | FC        | II & III
+19AI539     | FC        | II & III
+19AI540     | FC        | II & III
+19AI541     | SBC       | II & III
+19AI543     | FC        | II & III
+19AI545     | FC        | II & III
+19AI547     | FC        | II & III
+19AI550     | FC        | II & III
+19AI553     | FC        | II & III
+19AI555     | FC        | II & III
+19AI603     | FC        | II & III
+19AI604     | FC        | II & III
+19AI801     | FC        | II & III
+19AM401     | FC        | II & III
+19AM508     | FC        | II & III
+19AM509     | FC        | II & III
+19BM401     | FC        | II & III
+19BM407     | FC        | II & III
+19BM408     | SBC       | II & III
+19BM412     | FC        | II & III
+19BM501     | FC        | II & III
+19BM519     | FC        | II & III
+19BM520     | FC        | II & III
+19BM809     | SBC       | II & III
+19BY202     | FC        | II & III
+19CE303     | FC        | II & III
+19CE406     | FC        | II & III
+19CE414     | FC        | II & III
+19CE415     | SBC       | II & III
+19CE416     | SBC       | II & III
+19CE418     | FC        | II & III
+19CE539     | FC        | II & III
+19CE547     | FC        | II & III
+19CE701     | SBC       | II & III
+19CE807     | FC        | II & III
+19CH406     | SBC       | II & III
+19CH409     | FC        | II & III
+19CH412     | SBC       | II & III
+19CH413     | FC        | II & III
+19CH415     | SBC       | II & III
+19CH522     | FC        | II & III
+19CH552     | FC        | II & III
+19CH701     | SBC       | II & III
+19CS304     | FC        | II & III
+19CS305     | FC        | II & III
+19CS404     | SBC       | II & III
+19CS405     | SBC       | II & III
+19CS406     | SBC       | II & III
+19CS407     | FC        | II & III
+19CS408     | FC        | II & III
+19CS409     | FC        | II & III
+19CS415     | SBC       | II & III
+19CS416     | FC        | II & III
+19CS417     | FC        | II & III
+19CS418     | FC        | II & III
+19CS419     | FC        | II & III
+19CS420     | FC        | II & III
+19CS421     | FC        | II & III
+19CS504     | FC        | II & III
+19CS509     | FC        | II & III
+19CS529     | FC        | II & III
+19CS542     | FC        | II & III
+19CS543     | FC        | II & III
+19CS545     | FC        | II & III
+19CS547     | FC        | II & III
+19CS549     | FC        | II & III
+19CS565     | FC        | II & III
+19CS570     | FC        | II & III
+19CS579     | FC        | II & III
+19CS580     | FC        | II & III
+19CS581     | FC        | II & III
+19CY205     | FC        | II & III
+19CY208     | FC        | II & III
+19CY801     | FC        | II & III
+19CY802     | FC        | II & III
+19CY805     | FC        | II & III
+19EC302     | FC        | II & III
+19EC305     | FC        | II & III
+19EC306     | FC        | II & III
+19EC307     | FC        | II & III
+19EC308     | FC        | II & III
+19EC401     | FC        | II & III
+19EC402     | FC        | II & III
+19EC404     | FC        | II & III
+19EC406     | SBC       | II & III
+19EC408     | SBC       | II & III
+19EC409     | SBC       | II & III
+19EC410     | SBC       | II & III
+19EC411     | FC        | II & III
+19EC412     | FC        | II & III
+19EC413     | FC        | II & III
+19EC415     | FC        | II & III
+19EC419     | SBC       | II & III
+19EC420     | FC        | II & III
+19EC421     | SBC       | II & III
+19EC424     | SBC       | II & III
+19EC503     | FC        | II & III
+19EC507     | FC        | II & III
+19EC511     | FC        | II & III
+19EC519     | FC        | II & III
+19EC520     | FC        | II & III
+19EC521     | FC        | II & III
+19EC522     | FC        | II & III
+19EC524     | FC        | II & III
+19EC526     | FC        | II & III
+19EC603     | FC        | II & III
+19EC604     | FC        | II & III
+19EC605     | FC        | II & III
+19EC606     | FC        | II & III
+19EC801     | SBC       | II & III
+19EC802     | FC        | II & III
+19EC803     | FC        | II & III
+19EC806     | FC        | II & III
+19EC819     | FC        | II & III
+19EC820     | FC        | II & III
+19EE301     | SBC       | II & III
+19EE305     | FC        | II & III
+19EE306     | FC        | II & III
+19EE308     | FC        | II & III
+19EE309     | FC        | II & III
+19EE311     | FC        | II & III
+19EE312     | FC        | II & III
+19EE404     | FC        | II & III
+19EE407     | FC        | II & III
+19EE412     | FC        | II & III
+19EE413     | FC        | II & III
+19EE414     | FC        | II & III
+19EE415     | FC        | II & III
+19EE416     | SBC       | II & III
+19EE511     | FC        | II & III
+19EE512     | FC        | II & III
+19EE532     | FC        | II & III
+19EE806     | FC        | II & III
+19EE830     | FC        | II & III
+19EI404     | FC        | II & III
+19EI407     | FC        | II & III
+19EI411     | SBC       | II & III
+19EI413     | SBC       | II & III
+19EI414     | SBC       | II & III
+19EI416     | FC        | II & III
+19EI417     | FC        | II & III
+19EN101     | SBC       | II & III
+19EN106     | SBC       | II & III
+19EN107     | SBC       | II & III
+19EN609     | FC        | II & III
+19EN616     | SBC       | II & III
+19EN618     | FC        | II & III
+19EN624     | SBC       | II & III
+19EY708     | SBC       | II & III
+19EY709     | FC        | II & III
+19EY710     | FC        | II & III
+19EY711     | FC        | II & III
+19HS602     | FC        | II & III
+19HS801     | FC        | II & III
+19IT406     | FC        | II & III
+19IT505     | FC        | II & III
+19MA201     | FC        | II & III
+19MA204     | FC        | II & III
+19MA211     | FC        | II & III
+19MA217     | FC        | II & III
+19MA219     | FC        | II & III
+19MA220     | FC        | II & III
+19MA222     | FC        | II & III
+19MA223     | FC        | II & III
+19MA224     | FC        | II & III
+19MD402     | FC        | II & III
+19MD403     | FC        | II & III
+19ME306     | FC        | II & III
+19ME410     | FC        | II & III
+19ME411     | SBC       | II & III
+19ME415     | FC        | II & III
+19ME503     | FC        | II & III
+19ME509     | FC        | II & III
+19ME521     | SBC       | II & III
+19ME529     | FC        | II & III
+19ME531     | FC        | II & III
+19ME533     | FC        | II & III
+19ME535     | SBC       | II & III
+19ME536     | FC        | II & III
+19ME571     | SBC       | II & III
+19ME573     | SBC       | II & III
+19ME701     | SBC       | II & III
+19MS154     | FC        | II & III
+19MS155     | FC        | II & III
+19MS156     | FC        | II & III
+19PH209     | FC        | II & III
+19PH211     | FC        | II & III
+19PH213     | FC        | II & III
+19PH214     | FC        | II & III
+19PH215     | FC        | II & III
+19PH216     | FC        | II & III
+19PH601     | FC        | II & III
+19PH602     | FC        | II & III
+19PH802     | FC        | II & III
+19PH814     | FC        | II & III
+19TD603     | FC        | II & III
+19TD605     | FC        | II & III
+19TD608     | FC        | II & III
+19TD609     | FC        | II & III
+19CH803     | FC        | II & III
+19MD406     | SBC       | II & III
+19MA213     | FC        | II & III
+19CH404     | FC        | II & III
+19ME407     | SBC       | II & III
+19ME403     | SBC       | II & III
+19ME405     | SBC       | II & III`;
